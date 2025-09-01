b748a26efe5aaef5ca4822bf169789689481e640
Alumno Torre Tecnológica <alu_torre1@instructores.senati.local>
2025-09-01 11:12:53 -0500

    feat(pacientes): habilitar formulario para registrar pacientes en la BD

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,18 @@
 <?php
 
+use App\Http\Controllers\Api\PacienteController;
 use Illuminate\Support\Facades\Route;
 
 // Route::get('/', function () {
 //     return view('welcome');
 // });
 
-Route::get('/login', function(){
+Route::get('/login', function () {
     return view('auth.login');
 })->name('login');
+
+Route::get('/patients', function () {
+    return view('patients.formulario');
+})->name('patients.formulario');
+
+Route::post('/patients', [PacienteController::class, 'store'])->name('patients.store');
